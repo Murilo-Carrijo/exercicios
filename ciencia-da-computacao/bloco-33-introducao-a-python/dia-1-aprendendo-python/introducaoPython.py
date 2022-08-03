728d107befa89df59f38f742539ac53e28e59995
murilo <murilocarrijoadm@gmail.com>
2022-08-03 19:14:52 -0300

    exercicio 3 - printat um quadrado de * baseado no numero passado por parametro

--- ciencia-da-computacao/bloco-33-introducao-a-python/dia-1-aprendendo-python/introducaoPython.py
+++ ciencia-da-computacao/bloco-33-introducao-a-python/dia-1-aprendendo-python/introducaoPython.py
@@ -20,3 +20,12 @@ def average(listNumbers):
  
 numbers = [10, 20, 30]
 average(numbers)
+
+# Exercício 3: Faça um programa que, dado um valor n qualquer, tal que n > 1, imprima na tela um quadrado feito de asteriscos de lado de tamanho n. Por exemplo
+
+def squaresDrawing(n):
+    for row in range(n):
+        print(n * '*')
+
+
+squaresDrawing(5)
