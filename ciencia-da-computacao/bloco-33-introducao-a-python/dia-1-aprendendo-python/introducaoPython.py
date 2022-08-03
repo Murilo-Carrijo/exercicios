2035cdfb9cc5f6996ab3118858a77693c6743296
murilo <murilocarrijoadm@gmail.com>
2022-08-03 19:12:32 -0300

    exercicio 1 - identificando o maior número

---
+++ ciencia-da-computacao/bloco-33-introducao-a-python/dia-1-aprendendo-python/introducaoPython.py
@@ -0,0 +1,11 @@
+# 🚀 Exercício 1: Crie uma função que receba dois números e retorne o maior deles.
+
+
+def higherNumber(x, y):
+    if x > y:
+        return x
+    return y
+
+
+higherNumber(2, 3)
+
