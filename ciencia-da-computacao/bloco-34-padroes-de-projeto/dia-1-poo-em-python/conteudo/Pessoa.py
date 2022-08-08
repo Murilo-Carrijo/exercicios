426157508a8f1e80e8ff7b35969e358e68b8ea16
murilo <murilocarrijoadm@gmail.com>
2022-08-08 18:10:13 -0300

    implementando exercicios de POO apresentados no conteudo da aula

---
+++ ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-1-poo-em-python/conteudo/Pessoa.py
@@ -0,0 +1,12 @@
+from conteudo.Eletrodomestico import Eletrodomestico
+
+class Pessoa:
+    def __init__(self, nome, saldo_na_conta):
+        self.nome = nome
+        self.saldo_na_conta = saldo_na_conta
+        self.eletrodomesticos = []
+    
+    def comprar_eletrodomestico(self, eletrodomestico: Eletrodomestico):
+        if eletrodomestico.preco >= self.saldo_na_conta:
+            self.saldo_na_conta -= eletrodomestico.preco
+            self.eletrodomesticos.append(eletrodomestico)
